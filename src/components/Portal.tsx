2d17444d16cb595767355c530bcafd917e20ffd4
Scott Dodge <scott@scotato.com>
2021-11-22 14:45:30 -0800

    tweak colors

--- src/components/Portal.tsx
+++ src/components/Portal.tsx
@@ -1,21 +1,21 @@
 // import { useEffect, useState } from "react";
 import styled from "styled-components";
 
-const colors = [
-  "#838383",
-  "#2E82FF",
-  "#C13CFF",
-  "#00DC82",
-  "#F8B73E",
-  "#FF44B7",
-];
+// const colors = [
+//   "#838383",
+//   "#2E82FF",
+//   "#C13CFF",
+//   "#00DC82",
+//   "#F8B73E",
+//   "#FF44B7",
+// ];
 
 export const Portal = () => {
   return (
     <SVG
       viewBox={`0 0 ${48} ${48}`}
       xmlns="http://www.w3.org/2000/svg"
-      fill={colors[5]}
+      // fill={colors[5]}
     >
       <PortalQuarter quadrant={1} />
       <PortalQuarter quadrant={2} />
@@ -33,43 +33,43 @@ const PortalQuarter = ({ quadrant }: { quadrant: number }) => {
       )}) translate(${translateByQuadrant(quadrant)})`}
     >
       <rect width="24" height="24" className="bg" />
-      <rect opacity="0.5" x="0" y="23" />
-      <rect opacity="0.75" x="20" y="2" />
-      <rect opacity="0.75" x="20" y="3" />
-      <rect opacity="0.75" x="21" y="2" />
-      <rect opacity="0.75" x="21" y="3" />
-      <rect opacity="0.4" x="20" y="23" />
-      <rect opacity="0.4" x="21" y="23" />
-      <rect opacity="0.4" x="0" y="2" />
-      <rect opacity="0.4" x="0" y="3" />
-      <rect opacity="0.75" x="5" y="3" />
-      <rect opacity="0.75" x="5" y="4" />
-      <rect opacity="0.75" x="6" y="3" />
-      <rect opacity="0.75" x="6" y="4" />
-      <rect opacity="0.4" x="10" y="5" />
-      <rect opacity="0.4" x="10" y="6" />
-      <rect opacity="0.4" x="11" y="5" />
-      <rect opacity="0.4" x="11" y="6" />
-      <rect opacity="0.4" x="17" y="12" />
-      <rect opacity="0.4" x="17" y="13" />
-      <rect opacity="0.4" x="18" y="12" />
-      <rect opacity="0.4" x="18" y="13" />
-      <rect opacity="0.75" x="19" y="17" />
-      <rect opacity="0.75" x="19" y="18" />
-      <rect opacity="0.75" x="20" y="17" />
-      <rect opacity="0.75" x="20" y="18" />
-      <rect opacity="0.75" x="14" y="8" />
-      <rect opacity="0.75" x="14" y="9" />
-      <rect opacity="0.75" x="15" y="8" />
-      <rect opacity="0.75" x="15" y="9" />
-      <rect opacity="0.5" x="15" y="3" />
-      <rect opacity="0.5" x="15" y="4" />
-      <rect opacity="0.5" x="16" y="3" />
-      <rect opacity="0.5" x="16" y="4" />
-      <rect opacity="0.5" x="19" y="7" />
-      <rect opacity="0.5" x="19" y="8" />
-      <rect opacity="0.5" x="20" y="7" />
-      <rect opacity="0.5" x="20" y="8" />
+      <rect className="shade-half" x="0" y="23" />
+      <rect className="shade-quarter" x="20" y="2" />
+      <rect className="shade-quarter" x="20" y="3" />
+      <rect className="shade-quarter" x="21" y="2" />
+      <rect className="shade-quarter" x="21" y="3" />
+      <rect className="shade-half" x="20" y="23" />
+      <rect className="shade-half" x="21" y="23" />
+      <rect className="shade-half" x="0" y="2" />
+      <rect className="shade-half" x="0" y="3" />
+      <rect className="shade-quarter" x="5" y="3" />
+      <rect className="shade-quarter" x="5" y="4" />
+      <rect className="shade-quarter" x="6" y="3" />
+      <rect className="shade-quarter" x="6" y="4" />
+      <rect className="shade-half" x="10" y="5" />
+      <rect className="shade-half" x="10" y="6" />
+      <rect className="shade-half" x="11" y="5" />
+      <rect className="shade-half" x="11" y="6" />
+      <rect className="shade-half" x="17" y="12" />
+      <rect className="shade-half" x="17" y="13" />
+      <rect className="shade-half" x="18" y="12" />
+      <rect className="shade-half" x="18" y="13" />
+      <rect className="shade-quarter" x="19" y="17" />
+      <rect className="shade-quarter" x="19" y="18" />
+      <rect className="shade-quarter" x="20" y="17" />
+      <rect className="shade-quarter" x="20" y="18" />
+      <rect className="shade-quarter" x="14" y="8" />
+      <rect className="shade-quarter" x="14" y="9" />
+      <rect className="shade-quarter" x="15" y="8" />
+      <rect className="shade-quarter" x="15" y="9" />
+      <rect className="shade-half" x="15" y="3" />
+      <rect className="shade-half" x="15" y="4" />
+      <rect className="shade-half" x="16" y="3" />
+      <rect className="shade-half" x="16" y="4" />
+      <rect className="shade-half" x="19" y="7" />
+      <rect className="shade-half" x="19" y="8" />
+      <rect className="shade-half" x="20" y="7" />
+      <rect className="shade-half" x="20" y="8" />
       <rect x="14" y="17" />
       <rect x="14" y="18" />
       <rect x="15" y="17" />
@@ -229,9 +229,20 @@ const SVG = styled.svg`
   margin: 0 auto;
 
   rect:not(.bg) {
-    fill: ${(props) => props.fill};
+    // fill: ${(props) => props.fill};
+    fill: hsl(323deg 100% 50%);
     width: 1.1px;
     height: 1.1px;
+  }
+
+  rect.shade-half {
+    // opacity: 0.5;
+    fill: hsl(323deg 100% 20%);
+  }
+
+  rect.shade-quarter {
+    // opacity: 0.75;
+    fill: hsl(323deg 100% 35%);
   }
 
   .bg {

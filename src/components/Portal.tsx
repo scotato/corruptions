d54bdc1982bbfddef85471fac731a9e0246c1350
Scott Dodge <scott@scotato.com>
2021-11-22 15:58:09 -0800

    add corruption logic

--- src/components/Portal.tsx
+++ src/components/Portal.tsx
@@ -1,21 +1,76 @@
 import styled from "styled-components";
-import portal from "../portal.json";
+import portalJSON from "../portal.json";
+const portalPixels = portalJSON as Pixels;
 
-export const Portal = () => {
+type X = number;
+type Y = number;
+type Shade = number;
+type PixelProperties = [X, Y, Shade];
+type Pixels = PixelProperties[];
+
+type PortalProps = {
+  pixels?: Pixels;
+  corruption?: number;
+};
+
+type PortalQuadrantProps = {
+  pixels: Pixels;
+  quadrant: number;
+};
+
+export const Portal = ({
+  corruption = 0,
+  pixels = portalPixels,
+}: PortalProps) => {
   return (
     <SVG viewBox={`0 0 48 48`} xmlns="http://www.w3.org/2000/svg">
-      <PortalQuarter quadrant={1} />
-      <PortalQuarter quadrant={2} />
-      <PortalQuarter quadrant={3} />
-      <PortalQuarter quadrant={4} />
+      <PortalQuadrant
+        quadrant={1}
+        pixels={corruptPixels({ corruption, pixels })}
+      />
+      <PortalQuadrant
+        quadrant={2}
+        pixels={corruptPixels({ corruption, pixels })}
+      />
+      <PortalQuadrant
+        quadrant={3}
+        pixels={corruptPixels({ corruption, pixels })}
+      />
+      <PortalQuadrant
+        quadrant={4}
+        pixels={corruptPixels({ corruption, pixels })}
+      />
     </SVG>
   );
 };
 
-const PortalQuarter = ({ quadrant }: { quadrant: number }) => {
+function corruptPixels({ corruption, pixels }: PortalProps) {
+  const length = pixels?.length ?? 0;
+  console.log(length, Math.round(length * 0.5));
+  // ensure at least 25% pixels remain
+  // return pixels ? pixels : [];
+  return pixels ? shuffleSlice(pixels) : [];
+}
+
+function shuffleSlice(pixels: Pixels) {
+  return shuffleArray(pixels).slice(0, pixels.length * 0.5);
+}
+
+function shuffleArray(array: any[]) {
+  let arr = [...array];
+  // tslint:disable-next-line:no-increment-decrement
+  for (let i = arr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+
+  return arr;
+}
+
+const PortalQuadrant = ({ quadrant, pixels }: PortalQuadrantProps) => {
   return (
     <g transform={`scale(${scaleByQuadrant(quadrant)}) translate(24 0)`}>
-      {portal.map(([x, y, shade]) => (
+      {pixels.map(([x, y, shade]) => (
         <rect x={x} y={y} className={shadeByFloat(shade)} key={`${x},${y}`} />
       ))}
     </g>
